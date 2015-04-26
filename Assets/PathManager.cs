372fca818615dd1acb1dde1fb4b4170cfdbf54c6
valevalorin <S510243@NW34974.nwmissouri.edu>
2015-04-26 15:37:24 -0500

    Enemies shit is working

---
+++ Assets/PathManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathManager : MonoBehaviour {
+
+	public Transform[] CommPath1;
+	public Transform[] CommPath1Rev;
+	public Transform[] CommPath2;
+	public Transform[] CommPath2Rev;
+
+	public Transform[] SoldPath1;
+	public Transform[] SoldPath1Rev;
+	public Transform[] SoldPath2;
+	public Transform[] SoldPath2Rev;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
