b4212cc6743d6c1da62d4539ec79631a3de19eee
ClaytonG <cmrg9175@gmail.com>
2015-04-27 00:15:09 -0500

    I did some stuff, finished the main menu

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class GameData : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	//Instance of the object
+	public static GameData Instance
+	{
+		get{ return instance; }
+		set{}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	private static GameData instance = null;
+
+	//Determine whether or not the object already exists
+	void Awake()
+	{
+		if(instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			DestroyImmediate(this.gameObject);
+		}
 	}
+
+	//The selected Skybox from the Menu scene
+	public Material selectedSkybox{get; set;}
 }
