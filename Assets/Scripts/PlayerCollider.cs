532b9271572617962a825ca250337669a4ec9d1e
dnguyenchevy <dnguyenchevy@live.com>
2015-04-27 07:18:16 -0500

    Refactor and force resolution to only 16:8 ratio resolution

--- Assets/Scripts/PlayerCollider.cs
+++ Assets/Scripts/PlayerCollider.cs
@@ -5,7 +5,9 @@ public class PlayerCollider : MonoBehaviour {
 
 	//Player has been hit by an object
 	void OnTriggerEnter(Collider collision){
-		if(!collision.gameObject.CompareTag("PlayerBullet"))
+		if (!collision.gameObject.CompareTag ("PlayerBullet")) {
+			Debug.Log ("I've been hit!");
 			gameObject.GetComponentInParent<GalagaSpaceship> ().Hit ();
+		}
 	}
 }
